e1e586feee93f886f76cdd0a22b0aa45bc333731
Nate Sanchez <30782279+natesanchez314@users.noreply.github.com>
2024-05-09 22:00:59 -0500

    Create an IOS project
    Might as well learn swift and IOS app development too!

---
+++ ios/littlelemmyapp/littlelemmyapp/littlelemmyappApp.swift
@@ -0,0 +1,17 @@
+//
+//  littlelemmyappApp.swift
+//  littlelemmyapp
+//
+//  Created by cdm on 5/9/24.
+//
+
+import SwiftUI
+
+@main
+struct littlelemmyappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
